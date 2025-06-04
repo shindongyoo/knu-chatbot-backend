f4dc082ec7b531dccf0f85dad95b55f4d8b8af6b
shindongyoo <gobdy0425@gmail.com>
2025-06-05 01:43:32 +0900

    code

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ r = redis.Redis(
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://knu-chatbot.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
